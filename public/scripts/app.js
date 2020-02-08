918cf31c7f325a8fbb707eabf326110b30eb7270
Eina Onting <einers@outlook.com>
2020-02-08 12:51:52 -0800

    add leaflet + render leaflet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,18 @@
 $(() => {
+  // Leaflet Map
+  const myMap = L.map("map-create").setView([49.280571, -123.11378], 15);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
+    {
+      maxZoom: 19,
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: "mapbox/streets-v11"
+    }
+  ).addTo(myMap);
   // $.ajax({
   //   method: "GET",
   //   url: "/api/users"
