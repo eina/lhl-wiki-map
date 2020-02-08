896a3112aa81e1e9a396542ca2860eb9023bea99
kzcheng <kevinzifancheng@gmail.com>
2020-02-08 13:07:16 -0800

    Install cookie-parser and require it.

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
-const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
+const app           = express();
+const morgan        = require('morgan');
+const cookieParser  = require('cookie-parser');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
