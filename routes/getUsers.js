cda5fb2ad0ff5b8c6493ca6ee718a9da58552931
kzcheng <kevinzifancheng@gmail.com>
2020-02-08 14:45:13 -0800

    progress on getUsers.
    added /all path to get all users.
    
    added /:userID path to get a user given ID.

--- routes/getUsers.js
+++ routes/getUsers.js
@@ -20,17 +20,40 @@ module.exports = (db) => {
             .json({ error: err.message });
         });
     } else {
-      db.query(`SELECT * FROM users;`)
-        .then(data => {
-          const users = data.rows;
-          res.json(users);
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
+      return res.status(401).json('401: y u no login');
     }
   });
+
+  router.get("/all", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json(users);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:userID", (req, res) => {
+    let queryParams = [];
+    let queryString = `SELECT * FROM users `;
+
+    queryParams.push(req.params.userID);
+    queryString += `WHERE users.id = $${queryParams.length};`;
+
+    db.query(queryString, queryParams)
+      .then(data => {
+        res.json(data.rows[0]);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
