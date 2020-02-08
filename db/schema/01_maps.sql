742a62aa902ac5f25b3db324a9da00a823cf4d1d
kzcheng <kevinzifancheng@gmail.com>
2020-02-07 16:48:03 -0800

    Rename to u_id

--- db/schema/01_maps.sql
+++ db/schema/01_maps.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS maps CASCADE;
 
 CREATE TABLE maps (
   id serial PRIMARY KEY NOT NULL,
-  owner_id integer REFERENCES users (id),
+  u_id integer REFERENCES users (id),
   title varchar,
   created_at timestamp
 );
--- db/schema/04_edits.sql
+++ db/schema/04_edits.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS edits CASCADE;
 
 CREATE TABLE edits (
   id serial PRIMARY KEY NOT NULL,
-  user_id integer REFERENCES users (id),
+  u_id integer REFERENCES users (id),
   map_id integer REFERENCES maps (id),
   edited_at timestamp
 );
