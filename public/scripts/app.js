13688bb49d814e3b491de398d98f9ea3831fb13d
Eina Onting <einers@outlook.com>
2020-02-08 23:26:45 -0800

    create single map view

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,33 +1,33 @@
 $(() => {
   /* Leaflet Shared Map? */
   /* Leaflet: View Map With Points (on single-map.ejs) */
-  // const renderSingleMap = function() {
-  //   // render map
-  //   const myMap = L.map("leaflet-map").setView([49.280571, -123.11378], 15);
-  //   const yvrMap = L.tileLayer(
-  //     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
-  //     {
-  //       maxZoom: 19,
-  //       attribution:
-  //         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-  //         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-  //         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //       id: "mapbox/streets-v11"
-  //     }
-  //   );
-  //   yvrMap.addTo(singleMap);
-  //   // render markers
-  //   L.marker([49.280571, -123.11378])
-  //     .bindPopup("Hopefully details here")
-  //     .addTo(singleMap);
-  //   L.marker([49.282656, -123.126912])
-  //     .bindPopup("Hopefully details here")
-  //     .addTo(singleMap);
-  //   L.marker([49.285944, -123.134379])
-  //     .bindPopup("Hopefully details here")
-  //     .addTo(singleMap);
-  // };
-  // renderSingleMap();
+  const renderSingleMap = function() {
+    // render map
+    const myMap = L.map("leaflet-map").setView([49.280571, -123.11378], 15);
+    const yvrMap = L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
+      {
+        maxZoom: 19,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: "mapbox/streets-v11"
+      }
+    );
+    yvrMap.addTo(myMap);
+    // render markers
+    L.marker([49.280571, -123.11378])
+      .bindPopup("Hopefully details here")
+      .addTo(myMap);
+    L.marker([49.282656, -123.126912])
+      .bindPopup("Hopefully details here")
+      .addTo(myMap);
+    L.marker([49.285944, -123.134379])
+      .bindPopup("Hopefully details here")
+      .addTo(myMap);
+  };
+  renderSingleMap();
 
   /* Leaflet: Create Map (on map-form.ejs) */
   const createMap = L.map("leaflet-map").setView([49.280571, -123.11378], 15);
