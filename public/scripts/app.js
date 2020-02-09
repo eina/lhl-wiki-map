6cc81158d2e76c76b2f11425163e1bb0b5ba44b6
Eina Onting <einers@outlook.com>
2020-02-08 22:49:34 -0800

    style create map form page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,37 @@
 $(() => {
   /* Leaflet Shared Map? */
-  const yvrMap = L.tileLayer(
+  /* Leaflet: View Map With Points (on single-map.ejs) */
+  // const renderSingleMap = function() {
+  //   // render map
+  //   const myMap = L.map("leaflet-map").setView([49.280571, -123.11378], 15);
+  //   const yvrMap = L.tileLayer(
+  //     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
+  //     {
+  //       maxZoom: 19,
+  //       attribution:
+  //         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+  //         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  //         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //       id: "mapbox/streets-v11"
+  //     }
+  //   );
+  //   yvrMap.addTo(singleMap);
+  //   // render markers
+  //   L.marker([49.280571, -123.11378])
+  //     .bindPopup("Hopefully details here")
+  //     .addTo(singleMap);
+  //   L.marker([49.282656, -123.126912])
+  //     .bindPopup("Hopefully details here")
+  //     .addTo(singleMap);
+  //   L.marker([49.285944, -123.134379])
+  //     .bindPopup("Hopefully details here")
+  //     .addTo(singleMap);
+  // };
+  // renderSingleMap();
+
+  /* Leaflet: Create Map (on map-form.ejs) */
+  const createMap = L.map("leaflet-map").setView([49.280571, -123.11378], 15);
+  L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
     {
       maxZoom: 19,
@@ -10,40 +41,7 @@ $(() => {
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: "mapbox/streets-v11"
     }
-  );
-
-  /* Leaflet: View Map With Points (on single-map.ejs) */
-  const renderSingleMap = function() {
-    // render map
-    const singleMap = L.map("single-map").setView([49.280571, -123.11378], 15);
-    const yvrMap = L.tileLayer(
-      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
-      {
-        maxZoom: 19,
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: "mapbox/streets-v11"
-      }
-    );
-    yvrMap.addTo(singleMap);
-    // render markers
-    L.marker([49.280571, -123.11378])
-      .bindPopup("Hopefully details here")
-      .addTo(singleMap);
-    L.marker([49.282656, -123.126912])
-      .bindPopup("Hopefully details here")
-      .addTo(singleMap);
-    L.marker([49.285944, -123.134379])
-      .bindPopup("Hopefully details here")
-      .addTo(singleMap);
-  };
-  renderSingleMap();
-
-  /* Leaflet: Create Map (on map-form.ejs) */
-  const createMap = L.map("map-create").setView([49.280571, -123.11378], 15);
-  yvrMap.addTo(createMap);
+  ).addTo(createMap);
 
   const onMapClick = function(e) {
     const { lat, lng } = e.latlng;
@@ -91,7 +89,7 @@ $(() => {
     }
   };
 
-  createMap.on("click", onMapClick);
+  createMap.on("click touchstart", onMapClick);
 
   /* Edit Place Details */
 
