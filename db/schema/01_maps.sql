003235286eddf99327d5c5925fde72e6344487ea
kzcheng <kevinzifancheng@gmail.com>
2020-02-10 15:42:35 -0800

    Updated database to include lat and lng on maps. Also changed spelling of lat and lng.

--- db/schema/01_maps.sql
+++ db/schema/01_maps.sql
@@ -4,6 +4,8 @@ CREATE TABLE maps (
   id serial PRIMARY KEY NOT NULL,
   u_id integer REFERENCES users (id) ON DELETE CASCADE,
   title varchar,
-  created_at timestamp
+  created_at timestamp,
+  center_lat double precision,
+  center_lng double precision
 );
 
--- db/seeds/02_points.sql
+++ db/seeds/02_points.sql
@@ -1,48 +1,48 @@
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (1, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.280571, - 123.126912);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (1, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.096614);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (2, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.107128);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (2, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.284280, - 123.126912);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (3, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.096614);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (3, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.285944, - 123.107128);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (4, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.284280, - 123.113780);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (4, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.280625, - 123.126912);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (5, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.107128);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (5, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.280625, - 123.126912);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (6, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.285944, - 123.096614);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (6, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.107128);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (7, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.284280, - 123.126912);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (7, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.282656, - 123.113780);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (8, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.284280, - 123.107128);
 
-INSERT INTO points (map_id, title, detail, image_url, lat, long)
+INSERT INTO points (map_id, title, detail, image_url, lat, lng)
   VALUES (8, 'Title', 'Details are important.', 'https://source.unsplash.com/random/800x600', 49.285944, - 123.096614);
 
