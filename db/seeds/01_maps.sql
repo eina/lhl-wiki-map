38daf27d3de3bee1d113ee645ea5f51837c3c1d7
kzcheng <kevinzifancheng@gmail.com>
2020-02-11 14:04:46 -0800

    Added data for maps

--- db/seeds/01_maps.sql
+++ db/seeds/01_maps.sql
@@ -1,24 +1,24 @@
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (1, 'Alice Map 1', '2016-06-22 11:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (1, 'Alice Map 1', '2016-06-22 11:00', 49.280571, - 123.11378);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (1, 'Alice Map 2', '2020-01-02 14:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (1, 'Alice Map 2', '2020-01-02 14:00', 49.282656, - 123.126912);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (2, 'Kira Map 1', '2018-03-19 17:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (2, 'Kira Map 1', '2018-03-19 17:00', 49.28428, - 123.107128);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (2, 'Kira Map 2', '2017-07-16 04:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (2, 'Kira Map 2', '2017-07-16 04:00', 49.280625, - 123.133607);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (3, 'Christopher Map 1', '2011-04-12 03:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (3, 'Christopher Map 1', '2011-04-12 03:00', 49.282656, - 123.126912);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (3, 'Christopher Map 2', '2012-02-11 07:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (3, 'Christopher Map 2', '2012-02-11 07:00', 49.281297, - 123.096614);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (4, 'William Map 1', '2014-10-07 02:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (4, 'William Map 1', '2014-10-07 02:00', 49.282656, - 123.126912);
 
-INSERT INTO maps (u_id, title, created_at)
-  VALUES (4, 'William Map 2', '2019-11-06 01:00');
+INSERT INTO maps (u_id, title, created_at, center_lat, center_lng)
+  VALUES (4, 'William Map 2', '2019-11-06 01:00', 49.285944, - 123.134379);
 
