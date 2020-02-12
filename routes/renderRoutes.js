b6150073b3b6abafbf897749626bacca67cd7cc4
Eina Onting <einers@outlook.com>
2020-02-11 16:18:02 -0800

    uncomment login

--- routes/renderRoutes.js
+++ routes/renderRoutes.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const router = express.Router();
-const { getUserByID, getUserByEmail } = require("../lib/dataHelpers/users");
+const { getUserByID } = require("../lib/dataHelpers/users");
 const { getMaps } = require("../lib/dataHelpers/maps");
 
 module.exports = db => {
@@ -25,37 +25,36 @@ module.exports = db => {
   //   res.render("single-map");
   // });
 
-  // router.get("/users/:id", (req, res) => {
-  //   const currentUser = req.cookies && req.cookies.userID ? req.cookies.userID : null;
-  //   // let user;
-  //   return getUserByID(db, { id: req.params.id }).then(user => {
-  //     res.render("profile", { currentUser, user });
-  //     return user;
-  //   });
-  // });
+  router.get("/users/:id", (req, res) => {
+    const currentUser = req.cookies && req.cookies.userID ? req.cookies.userID : null;
+    // let user;
+    return getUserByID(db, { id: req.params.id }).then(user => {
+      res.render("profile", { currentUser, user });
+      return user;
+    });
+  });
 
   router.get("/maps/new", (req, res) => {
     res.render("map-form");
   });
 
-  // // Get user given a user email address
-  // router.post(`/login`, (req, res) => {
-  //   let queryParams = [];
-  //   let queryString = `SELECT * FROM users `;
+  router.post(`/login`, (req, res) => {
+    let queryParams = [];
+    let queryString = `SELECT * FROM users `;
 
-  //   queryParams.push(req.body.userEmail);
-  //   queryString += `WHERE users.email = $${queryParams.length};`;
+    queryParams.push(req.body.userEmail);
+    queryString += `WHERE users.email = $${queryParams.length};`;
 
-  //   return db
-  //     .query(queryString, queryParams)
-  //     .then(data => {
-  //       const result = data.rows[0];
-  //       res.cookie("userID", data.rows[0].id);
-  //       res.render("index", { user: { ...result, currentUser: req.cookies.user } });
-  //     })
-  //     .catch(err => {
-  //       res.status(500).json({ error: err.message });
-  //     });
-  // });
+    return db
+      .query(queryString, queryParams)
+      .then(data => {
+        const result = data.rows[0];
+        res.cookie("userID", data.rows[0].id);
+        res.render("index", { user: { ...result, currentUser: req.cookies.user } });
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
   return router;
 };
