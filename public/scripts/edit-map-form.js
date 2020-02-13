ccab0efd7f9722a1c46cfa4f57f22e0fb13f2d25
Eina Onting <einers@outlook.com>
2020-02-13 10:19:04 -0800

    implement renderPlaceForm

--- public/scripts/edit-map-form.js
+++ public/scripts/edit-map-form.js
@@ -1,8 +1,93 @@
 $(() => {
   /* Leaflet: View Map With Points (on single-map.ejs) */
 
-  const renderSingleMap = function() {
-    const { mapDetails } = $("#leaflet-map").data();
+  /* Edit Place Details */
+  const {
+    mapDetails: { mapID, lat: mapLat, lng: mapLng }
+  } = $("#leaflet-map").data();
+  const $editPlaceBtn = $(".edit-place-btn");
+  const $deletePlaceBtn = $(".delete-place-btn");
+
+  const renderEditPlaceForm = function(placeID) {
+    return `<form id="edit-${placeID}">
+        <div class="card-body">
+          <label class="form-group">
+            <span class="form-label">Name</span>
+            <input type="text" class="form-input" id="edit-place2-name" name="place-name" />
+          </label>
+          <label class="form-group">
+            <span class="form-label">Image URL</span>
+            <input type="text" class="form-input" id="edit-place2-img" name="place-img" />
+          </label>
+          <label class="form-group">
+            <span class="form-label">Description</span>
+            <textarea class="form-input" id="edit-place2-desc" rows="3" name="place-desc"></textarea>
+          </label>
+        </div>
+        <div class="card-footer">
+          <button class="btn edit-place" type="button">Edit</button>
+          <button class="btn" type="button" id="cancel-edit-${placeID}">Cancel</button>
+        </div>
+      </form>`;
+  };
+
+  const editPlace = function() {
+    const { pointId: pointID } = $(this).data();
+    // find the parent
+    const $parent = $(`[data-map=${mapID}][data-point=${pointID}]`);
+    // clone the element in case user cancels their edit
+    const $copyBeforeEdit = $parent.clone();
+
+    $parent.append(renderEditPlaceForm(pointID));
+  };
+
+  $editPlaceBtn.on("click", editPlace);
+
+  // editPlaceBtn.click(function(event) {
+  //   const $placeID = $(this)
+  //     .parent()
+  //     .parent();
+  //   const $placeIDString = $placeID.attr("id");
+  //   const $copyBeforeEdit = $placeID.clone();
+  //   // todo: need the details of this place to be populated!
+
+  // const renderEditPlaceForm = placeId => {
+  //   return `<form id="edit-${placeId}">
+  //     <div class="card-body">
+  //       <label class="form-group">
+  //         <span class="form-label">Name</span>
+  //         <input type="text" class="form-input" id="edit-place2-name" name="place-name" />
+  //       </label>
+  //       <label class="form-group">
+  //         <span class="form-label">Image URL</span>
+  //         <input type="text" class="form-input" id="edit-place2-img" name="place-img" />
+  //       </label>
+  //       <label class="form-group">
+  //         <span class="form-label">Description</span>
+  //         <textarea class="form-input" id="edit-place2-desc" rows="3" name="place-desc"></textarea>
+  //       </label>
+  //     </div>
+  //     <div class="card-footer">
+  //       <button class="btn edit-place" type="button">Edit</button>
+  //       <button class="btn" type="button" id="cancel-edit-${placeId}">Cancel</button>
+  //     </div>
+  //   </form>`;
+  // };
+
+  //   $placeID.empty();
+  //   $placeID.append(() => renderEditPlaceForm($placeIDString));
+
+  //   // todo: make this work lol
+  //   const revertChanges = function(event) {
+  //     $placeID.empty();
+  //     $placeID.append($copyBeforeEdit);
+  //   };
+
+  //   // click handler for cancel edit
+  //   $(`#cancel-edit-${$placeIDString}`).click(revertChanges);
+  // });
+
+  const renderSingleMap = function({ mapLat, mapLng }) {
     const $mapTitle = $("#map-title");
     const { value: titleValue } = $mapTitle.data();
 
@@ -12,9 +97,9 @@ $(() => {
     /**
      * Render Map
      */
-    if (mapDetails) {
+    if ((mapLat, mapLng)) {
       const { points } = $("#points-display").data();
-      const mapLatLng = L.latLng(mapDetails.lat, mapDetails.lng);
+      const mapLatLng = L.latLng(mapLat, mapLng);
       const myMap = L.map("leaflet-map").setView(mapLatLng, 14);
       const yvrMap = L.tileLayer(
         "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlbGl0dGxlYmxhY2tzbWl0aCIsImEiOiJjazZlMnExanYwaXU0M2tsb2I5cDRzcTQwIn0.bwS19as5AZCy7I-y3w-Tkw",
@@ -56,5 +141,5 @@ $(() => {
     }
   };
 
-  renderSingleMap();
+  renderSingleMap({ mapLat, mapLng });
 });
